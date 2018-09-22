02d80f9ef5f8c40a593bba5f94f79b07d6e9133d
hxm16 <hxm16@ic.ac.uk>
2018-09-22 17:23:50 +0800

    Added about, contact, projects page

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Me</h1>
+      <p>
+        I am a Year 3 Computing student who has developed a good level of problem solving problem solving skills as well
+        as enthusiasm for science and technology. I am eager to be challenged in order to grow and improve my
+        communication and practical knowledge in software engineering, and become an software engineer in the near future.
+      </p>
+    </div>
+  </section>
+</template>

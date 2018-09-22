243f95fae8ddbdaa5040c8a08f8473e816d745e0
hxm16 <hxm16@ic.ac.uk>
2018-09-22 20:33:18 +0800

    Added favico and logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
 <template>
-  <section class="section is-large">
+  <section class="section is-medium">
     <div class="container has-text-centered">
+      <img src="logo.png"/>
       <h1 class="title">Hi, I'm Ivan Mang</h1>
-      <h2 class="subtitle">a 3rd Year Computing Student in Imperial College London.</h2>
+      <h2 class="subtitle">a 3rd Year Computing Student</h2>
     </div>
   </section>
 </template>
+
+<style scoped>
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 5px;
+    width: 300px;
+  }
+</style>

849da2b3435c2438ffbe5dd5d74418b81efa2ba9
hxm16 <hxm16@ic.ac.uk>
2018-09-23 22:50:20 +0800

    Added basic CV

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,8 +1,33 @@
 <template>
-  <section class="section is-medium">
+  <section class="section is-small">
     <div class="container has-text-centered">
+      <div class="intro">
+        <img src="cv.jpg">
+        <h1 class="title">Ivan Mang</h1>
+        <p>I am a Year 3 Computing student who has developed a good level of problem solving problem solving skills as well
+          as enthusiasm for science and technology. I am eager to be challenged in order to grow and improve my
+          communication and practical knowledge in software engineering, and become an software engineer in the near future.</p>
+      </div>
+
 
     </div>
   </section>
 </template>
+
+<style scoped>
+  img {
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 150px;
+    width: 150px;
+  }
+  div.intro {
+    margin-left: auto;
+    margin-right: auto;
+    width: 31em;
+  }
+  h1.title {
+    line-height: 150%;
+  }
+</style>
 

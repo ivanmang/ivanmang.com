6749f791f48f616080461a592a0f2420fe06570e
hxm16 <hxm16@ic.ac.uk>
2018-09-24 16:40:15 +0800

    Added CV content

--- pages/cv.vue
+++ pages/cv.vue
@@ -5,7 +5,7 @@
         <h1 class="title">Ivan Mang</h1>
         <p>I am a Year 3 Computing student who has developed a good level of problem solving problem solving skills as well
           as enthusiasm for science and technology. I am eager to be challenged in order to grow and improve my
-          communication and practical knowledge in software engineering, and become an software engineer in the near future.</p>
+          communication and practical knowledge in software engineering, and become a software engineer in the near future.</p>
       </div>
     <hr>
     <div class="content">
@@ -35,6 +35,58 @@
         <h6>2009-2014</h6>
       </section>
     </div>
+    <hr>
+    <div class="content">
+      <h1 class="title"> Work Experience 🏢</h1>
+      <section class="section is-small">
+        <h2 class="subtitle">PwC HK</h2>
+        <h4>Trainee Assistant (Audit Methodology and Technology)</h4>
+        <h6>August 2018</h6>
+        <hr>
+        <h2 class="subtitle">Hopeson Investment Limited</h2>
+        <h4>Software Engineer Internship</h4>
+        <h6>July 2018</h6>
+        <hr>
+        <h2 class="subtitle">RICOH, UK</h2>
+        <h4>Trainee</h4>
+        <h6>April 2015</h6>
+      </section>
+    </div>
+    <hr>
+    <div class="content">
+      <h1 class="title"> Accomplishments 🏆</h1>
+      <section class="section is-small">
+        <h2 class="subtitle">Smart City Hackathon Hong Kong 2018</h2>
+        <h4>Champion of Smart Environment Challenge</h4>
+        <h6>September 2018</h6>
+        <hr>
+        <h2 class="subtitle">Global PropTech Ecosystem Mentorship Award</h2>
+        <h4>Asia PropTech Innovathon (Hong Kong round)</h4>
+        <h6>August 2018</h6>
+        <hr>
+        <h2 class="subtitle">DoCSoc (Department of Computing Society) Choice Award</h2>
+        <h4>ICHACK 18</h4>
+        <h6>January 2018</h6>
+      </section>
+    </div>
+    <hr>
+    <div class="content">
+      <h1 class="title"> Volunteer Experience ✋</h1>
+      <section class="section is-small">
+        <h2 class="subtitle">Imperial College Public Awareness and Social Service Society</h2>
+          <h4>Treasurer</h4>
+        <h6>2017-2018</h6>
+        <hr>
+        <h2 class="subtitle">Imperial Interact</h2>
+        <h4>Treasurer</h4>
+        <h6>2017-2018</h6>
+        <hr>
+        <h2 class="subtitle">Hong Kong Red Cross Youth Unit 39</h2>
+        <h4>Head Section Leader</h4>
+        <h6>2013-2014</h6>
+      </section>
+    </div>
+
   </section>
 </template>
 

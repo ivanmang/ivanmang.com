cf24bdea845e4722d1b61e6f845b9e9701a779db
hxm16 <hxm16@ic.ac.uk>
2018-09-24 14:12:33 +0800

    Completed About page

--- pages/about.vue
+++ pages/about.vue
@@ -9,14 +9,28 @@
       </div>
 
       <section class="section is-small">
+        <div class="content">
         <hr>
         <h1 class="subtitle">Innovative👨‍🎨</h1>
+          <p>He can always "think outside the box"💡 and suggesting innovative ideas to the team.</p>
+          <p>He initiated many science projects👨‍🔬 which joined many competitions and exhibitions since primary school.</p>
+          <p>He love joining Hackathon👨‍💻 and received many awards.</p>
         <hr>
         <h1 class="subtitle">Collaborative👯</h1>
+          <p>He can work well in a team and independently.</p>
+          <p>He likes getting involved in everything he interested🕵</p>
+          <p>He joined many societies as a committee members👨‍💼</p>
         <hr>
         <h1 class="subtitle">Hardworking👨‍💻</h1>
+          <p>He love solving problems and able to focus for very long time 🕒 (>24 hours)</p>
+          <p>He is always enthusiastic about entrepreneurship and finance </p>
+          <p>He is able to handled multiple tasks effectively📊</p>
         <hr>
         <h1 class="subtitle">Multi-talented🤹</h1>
+          <p>He love singing🎤, playing guitar🎸, piano🎹 and recorder in his spare time</p>
+          <p>He is a member of A-capella group🎼</p>
+          <p>He joined Red Cross and obtained First Aid and CPR Certificate🚑</p>
+        </div>
       </section>
 
 
@@ -29,5 +43,10 @@
       margin-left: auto;
       margin-right: auto;
       width: 50em;
+    }
+    div.content {
+      margin-left: auto;
+      margin-right: auto;
+      width: 60em;
     }
   </style>
